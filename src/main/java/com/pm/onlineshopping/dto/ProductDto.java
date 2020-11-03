32fbeb709e5e48d47b5ca11affe463f247da9ecb
Assefa <assefamit@gamil.com>
2020-11-03 05:47:37 -0600

    Eureka Client resolved issue

--- src/main/java/com/pm/onlineshopping/dto/ProductDto.java
+++ src/main/java/com/pm/onlineshopping/dto/ProductDto.java
@@ -20,7 +20,6 @@ import lombok.Setter;
 import lombok.ToString;
 
 @Data
-
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
--- src/main/java/com/pm/onlineshopping/service/ProductServiceImpl.java
+++ src/main/java/com/pm/onlineshopping/service/ProductServiceImpl.java
@@ -29,9 +29,7 @@ public class ProductServiceImpl implements ProductService {
 		Page<Product> products = productRepository.findByActiveTrue(pageable);
 		if (products.isEmpty())
 			throw new ProductNotFoundException("Empty record");
-		for(Product p: products) {
-			System.out.println(p.getCategory().getCategoryName());
-		}
+		
 		return products;
 	}
 
