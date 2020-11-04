94eb89bde216afd219a1eb6036ce99370636ca8f
Assefa <assefamit@gamil.com>
2020-11-04 13:34:53 -0600

    Kafka trusted package resolved

--- src/main/java/com/pm/onlineshopping/config/KafkaConfig.java
+++ src/main/java/com/pm/onlineshopping/config/KafkaConfig.java
@@ -72,8 +72,8 @@ public class KafkaConfig {
 		config.put(ConsumerConfig.GROUP_ID_CONFIG,"product_id");
 		config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 		config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
-		config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-		config.put(ConsumerConfig.SECURITY_PROVIDERS_CONFIG, "*");
+		//config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+		//config.put(ConsumerConfig.SECURITY_PROVIDERS_CONFIG, "*");
 		
 		return new DefaultKafkaConsumerFactory<>(config, new StringDeserializer(),
 							new JsonDeserializer<>(Order.class));
